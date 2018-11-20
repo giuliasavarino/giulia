c0c5aa01b037a27f8ba75ed8e17dcf6c327ff754
giuliasavarino <s5106876@bournemouth.ac.uk>
2018-11-20 15:42:01 +0000

    Initial Commit

---
+++ giulia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  giulia
+//
+//  Created by Giulia Savarino (s5106876) on 20/11/2018.
+//  Copyright © 2018 Giulia Savarino (s5106876). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
